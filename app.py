880b864a7de295d44e18aa127473b916c08e33f1
bzmby <behzadmoumbeini@gmail.com>
2022-07-19 17:31:53 +0200

    UPDATE app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return "Blog Home"
+
+#blue print (which is an app) for admin is made mod_admin and is imported below. and should be registered below too: 
+from mod_admin import admin
+
+
+app.register_blueprint(admin)
+
