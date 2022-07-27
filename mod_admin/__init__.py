569fe5edb7ed9144a10711a0895ad7ec128c50d3
bzmby <behzadmoumbeini@gmail.com>
2022-07-27 10:36:58 +0200

    separate views from __init__.py

--- mod_admin/__init__.py
+++ mod_admin/__init__.py
@@ -4,7 +4,5 @@ from flask import Blueprint
 admin = Blueprint('admin', __name__, url_prefix='/admin/')
 
 
-@admin.route('/')
-def admin_index():
-	return "hello from admin index"
 
+from .views import index
